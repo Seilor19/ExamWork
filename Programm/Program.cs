9a54faeffc71f5005c9b5679be30f0424332e175
Илья <seilormoonn@gmail.com>
2023-08-18 23:25:42 +0400

    Написанна программа

--- Programm/Program.cs
+++ Programm/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] FirstArray = new string[5] {"123", "23", "hello", "world", "res"};
+string[] SecondArray = new string[FirstArray.Length];
+
+
+void SecondArrayFunc(string[] FirstArray, string[] SecondArray)
+{
+    int count = 0;
+    for (int i = 0; i < FirstArray.Length; i++)
+    {
+    if(FirstArray[i].Length <= 3)
+        {
+        SecondArray[count] = FirstArray[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+
+SecondArrayFunc(FirstArray, SecondArray);
+PrintArray(SecondArray);
